743830db3e202a0ef51c2936dd884bb1986ddb12
Ramyamv24 <ramyavani689@gmail.com>
2026-06-22 16:59:56 +0530

    22-06-2026

---
+++ Binary Tree Level Order Traversal.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right)
+        : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> res;
+
+        if (!root)
+            return res;
+
+        queue<TreeNode*> q;
+        q.push(root);
+
+        while (!q.empty()) {
+            int size = q.size();
+            vector<int> level;
+
+            for (int i = 0; i < size; i++) {
+                TreeNode* node = q.front();
+                q.pop();
+
+                level.push_back(node->val);
+
+                if (node->left)
+                    q.push(node->left);
+
+                if (node->right)
+                    q.push(node->right);
+            }
+
+            res.push_back(level);
+        }
+
+        return res;
+    }
+};
+
+int main() {
+    // Creating the tree:
+    //       1
+    //      / \
+    //     2   3
+    //    / \   \
+    //   4   5   6
+
+    TreeNode* root = new TreeNode(1);
+
+    root->left = new TreeNode(2);
+    root->right = new TreeNode(3);
+
+    root->left->left = new TreeNode(4);
+    root->left->right = new TreeNode(5);
+
+    root->right->right = new TreeNode(6);
+
+    Solution obj;
+    vector<vector<int>> result = obj.levelOrder(root);
+
+    cout << "Level Order Traversal:\n";
+
+    for (auto level : result) {
+        for (int val : level) {
+            cout << val << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
